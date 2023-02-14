fe1397625b47ce77a843e15229c36b3e74023c30
xNachoMan <nathanielooi2005@gmail.com>
2023-02-15 10:10:20 +1100

    Update

--- main.py
+++ main.py
@@ -8,37 +8,74 @@ import pandas as pd
 from kivy.core.window import Window
 from kivymd.uix.dialog import MDDialog
 from kivymd.uix.button import MDFlatButton
+import numpy_financial as np
 
 
 class Calc(Screen):
     def on_enter(self):
         pass
 
-    def calc_values(self, leaseBudget, annualDistance, term1, term2, term3, term4, size1, size2, size3, size4, salaryResidual):
-        term, size = 0
-        print('\n\n'+str(int(leaseBudget)))
-        print(int(annualDistance))
+    def calc_values(self, leaseBudget, annualDistance, term1, term2, term3, term4, size1, size2, size3, size4,
+                    salaryResidual):
+        term = 0
+        print('\n\n' + "Lease Budget: " + str(int(leaseBudget)))
+        print("Distance: " + str(annualDistance))
         for i in (f'12 months {term1}', f'24 months {term2}', f'36 months {term3}', f'48 months {term4}'):
             if "True" in i:
-                print(str(i.replace("True", "")))
-                term = i
+                print("Term Length: " + str(i.replace("True", "")))
+                term = int(i[0:1])
 
         for i in (f'small {size1}', f'medium {size2}', f'large {size3}', f'sports {size4}'):
             if "True" in i:
-                print(str(i.replace("True", "")))
+                print("Car Size: " + str(i.replace("True", "")))
                 size = i
-        print(int(salaryResidual))
+        print("Salary: " + str(salaryResidual))
 
-        car_cost_GST = leaseBudget
+        car_cost_GST = float(leaseBudget)
         business_percentage = 0
-        taxable_income = salaryResidual
-        residual_value = ("65%", "56%", "46%", "37%")
-        kms_travelled_per_year = annualDistance
+        salary_income = float(salaryResidual)
+        residual_value = 0.4688
+        kms_travelled_per_year = float(annualDistance)
         lease_term = term
-        novated_interest_rate = "between 5% and 10%"
-        standard_interest_rate = "between 3% and 4%"
+        novated_interest_rate = 0.075
+        standard_interest_rate = 0.03
         monthly_fee = 20
-        car_size = size
+
+        aP = -(car_cost_GST - (car_cost_GST * residual_value))
+        ar = standard_interest_rate / 12
+        an = (lease_term * 12) - 2
+
+        print("Standard PMT: " + str(np.pmt(ar, an, aP) * 12))
+
+        normal_financing = np.pmt(ar, an, aP) * 12
+        normal_insurance = (car_cost_GST * 0.04)
+        normal_fees = 0
+        normal_registration = 800
+        normal_fuel = (((7.6 * (kms_travelled_per_year / 100)) * 1.5) * 1.2)
+        normal_maintenance = 1500
+
+        normal_total = normal_financing + normal_insurance + normal_fees + normal_registration + normal_fuel + normal_maintenance
+
+        car_cost_GST_adjusted = car_cost_GST - (car_cost_GST/11)
+
+        bP = -(car_cost_GST_adjusted - (car_cost_GST_adjusted * residual_value))
+        br = novated_interest_rate / 12
+        bn = (lease_term * 12) - 2
+
+        print("Novated PMT: " + str(np.pmt(br, bn, bP) * 12))
+
+        novated_financing = np.pmt(br, bn, bP) * 12
+        novated_insurance = normal_insurance - normal_insurance / 11
+        novated_fees = 12 * monthly_fee
+        novated_registration = normal_registration
+        novated_fuel = normal_fuel - normal_fuel / 11
+        novated_maintenance = normal_maintenance - normal_maintenance / 11
+
+        novated_total = novated_financing + novated_insurance + novated_fees + novated_registration + novated_fuel + novated_maintenance
+
+        # less_pre_tax_deduction = novated_total + (less_post_tax_deduction/11 - less_post_tax_deduction)
+        # less_post_tax_deduction =
+        # taxable_income = salary_income - less_pre_tax_deduction
 
 
 class Login(Screen):
@@ -56,7 +93,7 @@ class Login(Screen):
         if LoginUsername not in check['Username'].unique():
             print("Username not found")
             self.loginFail()
-            pass # deny access (Username not registered)
+            pass  # deny access (Username not registered)
         else:
             user_info = check[['Username', 'Password']][check['Username'] == LoginUsername]
             print(user_info)
@@ -95,7 +132,7 @@ class SignUp(Screen):
             if SignUpUsername and SignUpPassword1:
                 if SignUpUsername not in users['Username'].unique():
                     # if Username does not exist already then append to the csv file
-                    MDApp.get_running_app().switch_screen("login") # to change current screen to log in the user now
+                    MDApp.get_running_app().switch_screen("login")  # to change current screen to log in the user now
                     user.to_csv('login-details.csv', mode='a', header=False, index=False)
                     # self.SignUpUsername.text = ""
                     # self.SignUpPassword1.text = ""
--- test.py
+++ test.py
@@ -1,59 +1,20 @@
-class Piece:
-    def __init__(self, color):
-        self.color = color
+import numpy_financial as np
 
-    def can_move(self, x1, y1, x2, y2):
-        """Return True if the piece can move from (x1, y1) to (x2, y2), False otherwise."""
-        raise NotImplementedError
+car_cost_GST = 65000.00
+residual_value = 0.4688
+novated_interest_rate = 0.03
+lease_term = 3.00
 
+PMT = (car_cost_GST - (car_cost_GST * residual_value)) * (novated_interest_rate / 12) / (
+            1 - (1 + (novated_interest_rate / 12)) ** (-(lease_term * 12 - 2)))
 
-class Pawn(Piece):
-    def can_move(self, x1, y1, x2, y2):
-        if self.color == "white":
-            return (x1, y1 + 1) == (x2, y2)
-        else:
-            return (x1, y1 - 1) == (x2, y2)
+print(PMT*12)
 
 
-class Knight(Piece):
-    def can_move(self, x1, y1, x2, y2):
-        return ((x2, y2) in ((x1 + 2, y1 + 1), (x1 + 2, y1 - 1), (x1 - 2, y1 + 1), (x1 - 2, y1 - 1),
-                             (x1 + 1, y1 + 2), (x1 + 1, y1 - 2), (x1 - 1, y1 + 2), (x1 - 1, y1 - 2)))
+P = -(car_cost_GST - (car_cost_GST * residual_value))
+r = novated_interest_rate / 12
+n = (lease_term * 12) - 2
 
+print(np.pmt(r, n, P)*12)
 
-class Board:
-    def __init__(self):
-        self.board = [[None for _ in range(8)] for _ in range(8)]
-
-    def move_piece(self, x1, y1, x2, y2):
-        piece = self.board[x1][y1]
-        if piece and piece.can_move(x1, y1, x2, y2):
-            self.board[x2][y2] = piece
-            self.board[x1][y1] = None
-            return True
-        return False
-
-
-def play_game():
-    board = Board()
-
-    # Place pieces on the board
-    board.board[0][0] = Rook("white")
-    board.board[0][1] = Knight("white")
-    board.board[0][2] = Bishop("white")
-    # ...
-
-    while True:
-        # Display the board
-        for row in board.board:
-            print(" ".join("R" if piece else "-" for piece in row))
-        print()
-
-        # Get move from player
-        x1, y1, x2, y2 = map(int, input("Enter move (x1 y1 x2 y2): ").split())
-        if not board.move_piece(x1, y1, x2, y2):
-            print("Illegal move")
-
-
-if __name__ == '__main__':
-    play_game()
+# =(PMT(B11/12,(12*B9)-2,-(B4-(B4*B7))))*12
